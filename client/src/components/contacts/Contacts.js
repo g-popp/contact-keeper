1434dd020143b2f543c86a9b79ce1c93601977c9
Georg Popp <Georg.Popp@sovanta.com>
2021-11-05 15:14:29 +0100

    Contact Filter Form & State

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -5,16 +5,22 @@ import ContactItem from './ContactItem';
 const Contacts = () => {
 	const contactContext = useContext(ContactContext);
 
-	const { contacts } = contactContext;
+	const { contacts, filtered } = contactContext;
+
+	if (contacts.length === 0) {
+		return <h4>Please add a contact</h4>;
+	}
 
 	return (
-		<div>
-			<Fragment>
-				{contacts.map((contact) => (
-					<ContactItem key={contact.id} contact={contact} />
-				))}
-			</Fragment>
-		</div>
+		<Fragment>
+			{filtered !== null
+				? filtered.map((contact) => (
+						<ContactItem key={contact.id} contact={contact} />
+				  ))
+				: contacts.map((contact) => (
+						<ContactItem key={contact.id} contact={contact} />
+				  ))}
+		</Fragment>
 	);
 };
 
